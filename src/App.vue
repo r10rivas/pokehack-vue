01eab3f8dd266fe6ebd24652e605e29fb411cc46
Roberto Rivas <roberto.rivas.dev@gmail.com>
2020-07-25 15:28:06 -0400

    [#2] Add navigation and vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app">
-    <h1 class="text-red-700">Hola vale</h1>
+  <div class="flex flex-col items-center min-h-screen bg-app-white">
+    <div class="w-full
+                lg:w-478"
+    >
+
+      <Navegation/>
+      <router-view/>
+
+    </div>
   </div>
 </template>
 
 <script>
+  import Navegation from "./components/Navegation";
+
   export default {
-    name: 'App',
+    name: "App",
+    components: {
+      Navegation
+    }
   }
 </script>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about bg-red-800">
+    <h1>This is an about page</h1>
+  </div>
+</template>
