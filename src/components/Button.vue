4f9b5043fd91057edab0d8f7e4080fe334704f80
Roberto Rivas <roberto.rivas.dev@gmail.com>
2020-07-28 14:45:36 -0400

    [#6] Add page pokemon details

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="px-10 py-5 bg-blue-700 rounded-full text-white font-bold text-3xl"
+          style="minWidth: 140px"
+  >
+    {{label}}
+  </button>
+</template>
+<script>
+  export default {
+    name: "Button",
+    props: {
+      label: String
+    },
+  }
+</script>
